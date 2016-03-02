7a7c7dfd265fc26ca3de07817861da05c1ce6b9a
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-03-02 00:00:31 -0700

    Targets

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -4,14 +4,16 @@ $(function(){
   var canvas = document.querySelector('canvas');
   var ctx = canvas.getContext('2d');
   var launch = [];
-  var boxes = [ bx1 = [],  bx2 = [],  bx3 = [],  bx4 = [],  bx5 = []];
+  var boxes = [];
+  var targs = [];
   var tries = 15;
 
 
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  $('p').html('Tries ' + tries);
+
+  $('p').html('Tries: ' + tries);
 
   $(window).resize(function (){
     canvas.height = window.innerHeight;
@@ -27,7 +29,7 @@ $(function(){
       var randomw = Math.floor(Math.random()*(window.innerWidth*0.2+50));
       var randomh = Math.floor(Math.random()*(window.innerHeight*0.2+50));
 
-      boxes[i].push(randomx, randomy, randomw, randomh);
+      boxes.push([randomx, randomy, randomw, randomh]);
 
       ctx.fillStyle = 'rgba(0,255,255,0.5)';
       ctx.fillRect(boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]);
@@ -36,7 +38,26 @@ $(function(){
 
   }
 
+  function targets(){
+
+    for (var i = 0; i < 30; i++) {
+      var randomtx = Math.floor(Math.random()*window.innerWidth);
+      var randomty = Math.floor(Math.random()*(window.innerHeight*0.75-100));
+
+      targs.push([randomtx, randomty]);
+
+      ctx.beginPath();
+      ctx.arc(targs[i][0], targs[i][1], 10, 0, Math.PI*(2));
+      ctx.fillStyle = 'rgba(255,100,100,0.5)';
+      ctx.closePath();
+      ctx.fill();
+
+    }
+
+  }
+
   obstacles();
+  targets();
 
   function projectile(xpos,ypos){
     var xv = (xpos-launchx);
@@ -57,31 +78,31 @@ $(function(){
 
       // vertical collisions
 
-      if ( Math.floor(launchy) >= Math.floor(boxes[0][1]) && Math.floor(launchy) <= Math.floor(boxes[0][1] + boxes[0][3]) ) {
+      if ( Math.floor(launchy) == Math.floor(boxes[0][1]) || Math.floor(launchy) == Math.floor(boxes[0][1] + boxes[0][3]) ) {
         if ( Math.floor(launchx) >= Math.floor(boxes[0][0]) && Math.floor(launchx) <= Math.floor(boxes[0][0] + boxes[0][2]) ) {
           yv = -yv;
         }
       }
 
-      if ( Math.floor(launchy) >= Math.floor(boxes[1][1]) && Math.floor(launchy) <= Math.floor(boxes[1][1] + boxes[1][3]) ) {
+      if ( Math.floor(launchy) == Math.floor(boxes[1][1]) || Math.floor(launchy) == Math.floor(boxes[1][1] + boxes[1][3]) ) {
         if ( Math.floor(launchx) >= Math.floor(boxes[1][0]) && Math.floor(launchx) <= Math.floor(boxes[1][0] + boxes[1][2]) ) {
           yv = -yv;
         }
       }
 
-      if ( Math.floor(launchy) >= Math.floor(boxes[2][1]) && Math.floor(launchy) <= Math.floor(boxes[2][1] + boxes[2][3]) ) {
+      if ( Math.floor(launchy) == Math.floor(boxes[2][1]) || Math.floor(launchy) == Math.floor(boxes[2][1] + boxes[2][3]) ) {
         if ( Math.floor(launchx) >= Math.floor(boxes[2][0]) && Math.floor(launchx) <= Math.floor(boxes[2][0] + boxes[2][2]) ) {
           yv = -yv;
         }
       }
 
-      if ( Math.floor(launchy) >= Math.floor(boxes[3][1]) && Math.floor(launchy) <= Math.floor(boxes[3][1] + boxes[3][3]) ) {
+      if ( Math.floor(launchy) == Math.floor(boxes[3][1]) || Math.floor(launchy) == Math.floor(boxes[3][1] + boxes[3][3]) ) {
         if ( Math.floor(launchx) >= Math.floor(boxes[3][0]) && Math.floor(launchx) <= Math.floor(boxes[3][0] + boxes[3][2]) ) {
           yv = -yv;
         }
       }
 
-      if ( Math.floor(launchy) >= Math.floor(boxes[4][1]) && Math.floor(launchy) <= Math.floor(boxes[4][1] + boxes[4][3]) ) {
+      if ( Math.floor(launchy) == Math.floor(boxes[4][1]) || Math.floor(launchy) == Math.floor(boxes[4][1] + boxes[4][3]) ) {
         if ( Math.floor(launchx) >= Math.floor(boxes[4][0]) && Math.floor(launchx) <= Math.floor(boxes[4][0] + boxes[4][2]) ) {
           yv = -yv;
         }
@@ -89,51 +110,52 @@ $(function(){
 
       // horizontal collisions
 
-      // if ( Math.floor(launchx) >= Math.floor(boxes[0][0]) && Math.floor(launchx) <= Math.floor(boxes[0][0] + boxes[0][2]) ) {
-      //   console.log('y');
-      //   if ( Math.floor(launchy) >= Math.floor(boxes[0][1]) && Math.floor(launchy) <= Math.floor(boxes[0][1] + boxes[0][3]) ) {
-      //     xv = -xv;
-      //   }
-      // }
-      //
-      // if ( Math.floor(launchx) >= Math.floor(boxes[1][0]) && Math.floor(launchx) <= Math.floor(boxes[1][0] + boxes[1][2]) ) {
-      //   if ( Math.floor(launchy) >= Math.floor(boxes[1][1]) && Math.floor(launchy) <= Math.floor(boxes[1][1] + boxes[1][3]) ) {
-      //     xv = -xv;
-      //   }
-      // }
-      //
-      // if ( Math.floor(launchx) >= Math.floor(boxes[2][0]) && Math.floor(launchx) <= Math.floor(boxes[2][0] + boxes[2][2]) ) {
-      //   if ( Math.floor(launchy) >= Math.floor(boxes[2][1]) && Math.floor(launchy) <= Math.floor(boxes[2][1] + boxes[2][3]) ) {
-      //     xv = -xv;
-      //   }
-      // }
-      //
-      // if ( Math.floor(launchx) >= Math.floor(boxes[3][0]) && Math.floor(launchx) <= Math.floor(boxes[3][0] + boxes[3][2]) ) {
-      //   if ( Math.floor(launchy) >= Math.floor(boxes[3][1]) && Math.floor(launchy) <= Math.floor(boxes[3][1] + boxes[3][3]) ) {
-      //     xv = -xv;
-      //   }
-      // }
-      //
-      // if ( Math.floor(launchx) >= Math.floor(boxes[4][0]) && Math.floor(launchx) <= Math.floor(boxes[4][1] + boxes[4][2]) ) {
-      //   if ( Math.floor(launchy) >= Math.floor(boxes[4][1]) && Math.floor(launchy) <= Math.floor(boxes[4][1] + boxes[4][3]) ) {
-      //     xv = -xv;
-      //   }
-      // }
+      if ( Math.floor(launchx) == Math.floor(boxes[0][0]) || Math.floor(launchx) == Math.floor(boxes[0][0] + boxes[0][2]) ) {
+        console.log('y');
+        if ( Math.floor(launchy) >= Math.floor(boxes[0][1]) && Math.floor(launchy) <= Math.floor(boxes[0][1] + boxes[0][3]) ) {
+          xv = -xv;
+        }
+      }
+
+      if ( Math.floor(launchx) == Math.floor(boxes[1][0]) || Math.floor(launchx) == Math.floor(boxes[1][0] + boxes[1][2]) ) {
+        if ( Math.floor(launchy) >= Math.floor(boxes[1][1]) && Math.floor(launchy) <= Math.floor(boxes[1][1] + boxes[1][3]) ) {
+          xv = -xv;
+        }
+      }
+
+      if ( Math.floor(launchx) == Math.floor(boxes[2][0]) || Math.floor(launchx) == Math.floor(boxes[2][0] + boxes[2][2]) ) {
+        if ( Math.floor(launchy) >= Math.floor(boxes[2][1]) && Math.floor(launchy) <= Math.floor(boxes[2][1] + boxes[2][3]) ) {
+          xv = -xv;
+        }
+      }
+
+      if ( Math.floor(launchx) == Math.floor(boxes[3][0]) || Math.floor(launchx) == Math.floor(boxes[3][0] + boxes[3][2]) ) {
+        if ( Math.floor(launchy) >= Math.floor(boxes[3][1]) && Math.floor(launchy) <= Math.floor(boxes[3][1] + boxes[3][3]) ) {
+          xv = -xv;
+        }
+      }
+
+      if ( Math.floor(launchx) == Math.floor(boxes[4][0]) || Math.floor(launchx) == Math.floor(boxes[4][1] + boxes[4][2]) ) {
+        if ( Math.floor(launchy) >= Math.floor(boxes[4][1]) && Math.floor(launchy) <= Math.floor(boxes[4][1] + boxes[4][3]) ) {
+          xv = -xv;
+        }
+      }
 
 
       ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
       obstacles();
+      targets();
       ctx.beginPath();
       ctx.arc(launchx, launchy, 5, 0, Math.PI*(2));
       ctx.fillStyle = 'white';
       ctx.closePath();
       ctx.fill();
 
-      launchx += (xv/100);
-      launchy += (yv/100);
+      launchx += (xv/500);
+      launchy += (yv/500);
       loopcount += 1;
 
-      if (loopcount > 250){
+      if (loopcount > 1000){
         clearInterval(loopTimer);
         launchx = (window.innerWidth/2);
         launchy = (window.innerHeight);
@@ -141,7 +163,7 @@ $(function(){
 
       }
 
-    }, speedcount);
+    }, 1/200);
 
   }
 
@@ -149,12 +171,14 @@ $(function(){
     var xpos = e.pageX;
     var ypos = e.pageY;
 
-    if (launch.length == 0){
+    if (launch.length == 0 && tries != 0){
       launchx = (window.innerWidth/2);
       launchy = (window.innerHeight-1)
       projectile(xpos, ypos);
       tries -= 1;
-      $('p').html('Tries ' + tries);
+      $('p').html('Tries: ' + tries);
+
+
 
     }
 
